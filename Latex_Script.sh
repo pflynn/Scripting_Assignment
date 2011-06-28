bad6fbccdd8577a1504513ae159c17d12d7925f6
Patrick Flynn <pflynn@tssg.org>
2011-06-28 17:26:07 +0100

    Some unwanted strings added to Unwanted_Strings.txt, and Latex_Script has been modified such that it can ask for you to enter the filename if you have not specified, and it has a loop that can pick out the unwanted strings from Unwanted_Strings.txt

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -n "$1" ]
 # If command-line argument is present (non-empty), do the following.
 then
-  lines=$1
-  echo "arg"
+  inarg=$1 # The filename you enter is saved as variable "inarg"
 else  
 # If command-line argument is not present (empty), do the following.
-  lines=$LINES # Default, if not specified on command-line.
-  echo "no arg"
+  echo "Enter filename you wish to place in template"
+  read filename 
+  inarg=$filename # The filename you enter is saved as variable "inarg"
 fi  
+
+
+
+i=1 # Sets i to 1
+	  text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file
+	  	  
+	    until [ "$text" = "" ] # When this condition is met the loop has reached the end of the Unwanted_Strings.txt file and the loop aborts
+         do
+         echo $text # Echos the string in Unwanted_Strings.txt at line $i
+	     i=$(($i+1)) # increments i by 1
+		 text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the next string in the list in Unwanted_Strings.txt
+         done
+exit
