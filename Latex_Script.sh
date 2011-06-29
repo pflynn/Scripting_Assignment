283a7899ab37b607bcf87f5554e7c2ab889ae9b2
Patrick Flynn <pflynn@tssg.org>
2011-06-29 17:38:58 +0100

    trying to get strings to merge correctly

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -16,18 +16,29 @@ cp $inarg inputCopy.txt # Makes a copy of input file and names it inputCopy.txt
 i=1 # Sets i to 1
 
 
+	 # text=$(sed -e ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file
 
-	  text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file
-	  	  
+	  
+	  #text2=$(sed -n '/[$i]/p' Unwanted_Strings.txt)
+
+	  
+	 text="$(head -n $i Unwanted_Strings.txt)"
+	  
+	 # sed "/$text/ s|$| end|"
+	  
+	  
+	  
+	  
+	 echo "${text}"
+		  
 	     until [ "$text" = "" ] # When this condition is met the loop has reached the end of the Unwanted_Strings.txt file and the loop exits
          do
-           echo "/"$text # Echos the string in Unwanted_Strings.txt at line $i
-		  
-		   
-		   
 		 
-		   #sed "/"$text"/d" inputCopy.txt > tempfile.txt # Removes lines containing the string $text from inputCopy.txt and moves it to tempfile.txt
-		  # mv tempfile.txt inputCopy.txt # Overwrites inputCopy with tempfile which is a copy of inputCopy with a possible change made to it.                               
+		 
+		
+		 
+		    #sed "/${text}/d" inputCopy.txt > tempfile.txt  # Removes lines containing the string $text from inputCopy.txt and moves it to tempfile.txt
+		   # mv tempfile.txt inputCopy.txt # Overwrites inputCopy with tempfile which is a copy of inputCopy with a possible change made to it.                               
 	       i=$(($i+1)) # increments i by 1
 		   text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the next string in the list in Unwanted_Strings.txt
          done
