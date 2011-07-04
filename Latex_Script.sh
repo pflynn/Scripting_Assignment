ffdbd7c2d5cb911539d3b94cf8191fb84bff2306
Patrick Flynn <pflynn@tssg.org>
2011-07-04 10:09:38 +0100

    Changed Latex script, commented out stuff

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -16,30 +16,46 @@ cp $inarg inputCopy.txt # Makes a copy of input file and names it inputCopy.txt
 i=1 # Sets i to 1
 
 
-	 # text=$(sed -e ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file
-
-	  
-	  #text2=$(sed -n '/[$i]/p' Unwanted_Strings.txt)
-
-	  
-	 #text="$(head -n $i Unwanted_Strings.txt)"
-	  
+	 # text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file 
+	 # text=$(sed -n '/[$i]/p' Unwanted_Strings.txt)
+	# text="$(head -n $i Unwanted_Strings.txt)"  
 	 # sed "/$text/ s|$| end|"
 	  
-	  text1=$(grep ':' Unwanted_Strings.txt |-n1 -n2)
-	 
+	 # text=\"/"$(grep ':' Unwanted_Strings.txt |head -n1)" 
 	  
-	 echo "${text}" 
-		  
-	     until [ "$text" = "" ] # When this condition is met the loop has reached the end of the Unwanted_Strings.txt file and the loop exits
-         do
-		 
-		 
+	
+	##grep -v '^co$' inputCopy.txt >> inputCopy.txt
+	
 		
+		
+	
+	 #echo $text
+	#sed "/% coding: utf-8/d" inputCopy.txt > tempfile.txt
+	 
+		  
+	 #    until [ "$text" = "" ] # When this condition is met the loop has reached the end of the Unwanted_Strings.txt file and the loop exits
+     #   do
 		 
-		    #sed "/${text}/d" inputCopy.txt > tempfile.txt  # Removes lines containing the string $text from inputCopy.txt and moves it to tempfile.txt
-		   # mv tempfile.txt inputCopy.txt # Overwrites inputCopy with tempfile which is a copy of inputCopy with a possible change made to it.                               
-	       i=$(($i+1)) # increments i by 1
-		   text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the next string in the list in Unwanted_Strings.txt
-         done
+		 
+		 
+	#	echo "$text"
+		#sed -i "/$text/d" inputCopy.txt 
+		
+		#grep -Ev '$text|'
+		
+	#	eval "$text"
+		
+	#	cat inputCopy.txt | sed -i "$text"'d' > inputCopy.txt
+		
+		
+		 # eval "sed $text" # creates tempfile.txt? file for some reason instead of tempfile.txt
+		 # mv tempfile.txt? tempfile.txt # moves the tempfile.txt? to tempfile.txt
+		  
+		  #  sed "/${text}/d" inputCopy.txt > tempfile.txt  # Removes lines containing the string $text from inputCopy.txt and moves it to tempfile.txt
+	#	    mv tempfile.txt inputCopy.txt # Overwrites inputCopy with tempfile which is a copy of inputCopy with a possible change made to it.                               
+	 #     i=$(($i+1)) # increments i by 1
+	#	  text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt)  # Sets text to the next string in the list in Unwanted_Strings.txt
+      #   done
+	 ./Remove_Unwanted_Strings.sh 
+	  
 exit
