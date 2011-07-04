f51a07b7e93fde24acf14b3bfce0601c4109f957
Patrick Flynn <pflynn@tssg.org>
2011-07-04 15:21:59 +0100

    Added Comments, Removved variable inarg.

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -3,13 +3,11 @@
 if [ -n "$1" ]
 # If command-line argument is present (non-empty), do the following.
 then
-  inarg=$1 # The filename you enter is saved as variable "inarg"
-  filename=$inarg
+  filename=$1 # The filename you enter is saved as variable "filename" 
 else  
 # If command-line argument is not present (empty), do the following.
   echo "Enter filename you wish to place in template"
   read filename 
- # inarg=$filename # The filename you enter is saved as variable "inarg"
 fi  
 
 cp $filename inputCopy.txt # Makes a copy of input file and names it inputCopy.txt
@@ -19,12 +17,12 @@ i=1 # Sets i to 1
 	
 	     until [ "$text" = "" ] # When this condition is met the loop has reached the end of the Unwanted_Strings.txt file and the loop exits
            do	 
-             eval 'sed '$text''
-			   if [ -e "tempfile.txt" ]
+             eval 'sed '$text'' # Sed removes lines containing the variable $text from inputCopy.txt, and saves this in tempfile.txt or tempfile.txt?
+			   if [ -e "tempfile.txt" ] # If the file tempfile.txt is present
 			     then
-                 mv tempfile.txt inputCopy.txt 
-                 else
-				 mv tempfile.txt? inputCopy.txt 
+                 mv tempfile.txt inputCopy.txt # Overrides inputCopy.txt with tempfile.txt
+                 else # The file tempfile.txt? will be present if tempfile.txt is not.
+				 mv tempfile.txt? inputCopy.txt # Overrides inputCopy.txt with tempfile.txt?
                fi             
 	         i=$(($i+1)) # increments i by 1
 		     text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt)  # Sets text to the next string in the list in Unwanted_Strings.txt
