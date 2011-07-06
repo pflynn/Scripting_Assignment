10c71a070dbd99ef7f603d4aab1df64973d2bdf0
Patrick Flynn <pflynn@tssg.org>
2011-07-06 15:35:13 +0100

    Script now creates outputFile.tex, still tweeking to be done

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -31,49 +31,31 @@ cp whitepaper.tex whitepaper.txt # Converting whitepaper.tex to whitepaper.txt
 # Now must add remaining text to Template
 line_Num=$(wc -l < inputCopy.txt) # Number of lines in the inputCopy.txt file
 #string="T" # String at the point whare which we want to add text directly below
-pos=1; #Position of line being read in inputcopy.txt and being set to text
 while [ $line_Num -gt 0 ]
   do 
   
-  text=$(sed -n ''$pos'{p;q}' inputCopy.txt) # inputcopy.txt line pos being set to text
+  text=$(sed -n ''$line_Num'{p;q}' inputCopy.txt) # inputcopy.txt line pos being set to text
 # text="tes"
 	echo $line_Num
-	#echo $text | 
-  # sed '/^This.*/a\$text' whitepaper.txt > tempfile.txt''
-  
- # awk 'NR=='40'{print $text; print;next}1' whitepaper.txt > tempfile.txt
- 
- 
- #awk '{print $text}'
-  # sed -e '40 a\Add this line after 40th line'whitepaper.txt > tempfile.txt
-  
-  #cat -n inputCopy.txt
-  
- # sed '40'"a $text"'' whitepaper.txt > tempfile.txt
-
-#sed '$ 40 '"$text"'' whitepaper.txt > tempfile.txt
-
-#string=$(sed -n ''$(($line_Num+40))'{p;q}' whitepaper.txt)
-
+	
+# Places text below \input{template/title} in whitepaper template
 sed "
-/\\input/ a\
+/[\*]input{template\/title}/ a\
  ${text}
 " whitepaper.txt > tempfile.txt
 
   mv tempfile.txt whitepaper.txt
   
- 
-
 line_Num=$(($line_Num-1))
-pos=$(($pos+1))
-
 
 done
 
+# Remove the inputCopy.txt file
+rm inputCopy.txt
 
+# Move whitepaper.txt to outputFile.tex
+mv whitepaper.txt outputFile.tex
 
-#text= head -$(wc -l < inputCopy.txt) inputCopy.txt # Saves the text from inputCopy.txt to variable text
-#echo $text
 
 
 
