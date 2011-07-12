5479f5e51b32087df643a2ea4bf5fdd73b6a0e73
Patrick Flynn <pflynn@tssg.org>
2011-07-12 11:39:04 +0100

    Script now adding author name(s) to pdf

--- Latex_Script.sh
+++ Latex_Script.sh
@@ -14,8 +14,6 @@ fi
 cp $filename inputCopy.txt # Makes a copy of input file and names it inputCopy.txt
 # Now must pick out author names 
 authors=$(cat inputCopy.txt | grep author{ | awk -F{ '{print $2}' |awk -F} '{print $1}')
-echo $authors
-
 
 i=1 # Sets i to 1
 text=$(sed -n ''$i'{p;q}' Unwanted_Strings.txt) # Sets text to the first string in the Unwanted_Strings.txt file 
@@ -63,11 +61,13 @@ sed -i 's/par{}$//g' outputFile.tex # Removes all instances of par{} from lines 
 #sed -n -f sedCommands.sed outputFile.tex # Move all this to sed file
 sed -i 's/^oindent{}//g' outputFile.tex # Removes all instances of oindent{} from lines that start with oindent{} from outputFile.tex 
 sed -i "/^%$/d" outputFile.tex # Removes all lines that are just % from outputFile.tex 
+sed -i "s/David Muldowney, Christopher Foley, Steven Davy/$authors/g" outputFile.tex # Adding authors names to output file
+
 
 
 # Must replace all instances of % with \percent i.e. \\percent
 
 
 # Next step is use pdflatex to create outputFile.pdf from outputFile.tex
-#pdflatex outputFile.tex
+pdflatex outputFile.tex
 exit # Exits script
